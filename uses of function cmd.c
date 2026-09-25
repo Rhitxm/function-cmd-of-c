d5e567382864d6dd35cadbc8cb3b5e1ff154f10b
Rhitxm <shyamasree31@gmail.com>
2026-09-25 23:59:24 +0530

    Update uses of function cmd.c

--- uses of function cmd.c
+++ uses of function cmd.c
@@ -1,2 +1,25 @@
 //print hello and bonjour for different type of nationality
+#include <stdio.h>
+void namaste();
+void bonjour();
+
+int main(){
+    printf("enter your language, i for Indian and f for French");
+    char ch;
+    scanf("%c", &ch);
+    if(ch=='i'){
+        namaste();
+    }
+    else{
+        bonjour();
+    }
+   
+    return 0;
+}
+void namaste(){
+        printf("Namaste\n");
+    }
+void bonjour(){
+        printf("Bonjour\n");
+    }
 
