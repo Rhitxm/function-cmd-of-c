74b7768bc8dab01803e6bba67a8e1e70080e2317
Rhitxm <shyamasree31@gmail.com>
2026-09-26 11:51:27 +0530

    Update uses of function cmd.c

--- uses of function cmd.c
+++ uses of function cmd.c
@@ -22,4 +22,20 @@ void namaste(){
 void bonjour(){
         printf("Bonjour\n");
     }
+//two print sum of two  numbers using function cmd
+#include <stdio.h> 
+int sum(int a, int b);
+int main(){
+int a, b;
+printf("enter first number:\n");
+scanf("%d", &a);
+printf("enter second number:\n");
+scanf("%d", &b);
 
+int s=sum(a, b);
+    printf("sum is :%d\n", s);
+    return 0;
+}
+int sum(int x, int y){
+    return x+y;
+}
