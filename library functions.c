20f00dd687b21151ec4cebd3ca338fb170583071
Rhitxm <shyamasree31@gmail.com>
2026-09-26 23:49:58 +0530

    Update library functions.c

--- library functions.c
+++ library functions.c
@@ -1,1 +1,10 @@
-
+//using library functions to calculate square of a number given by user
+#include<stdio.h>
+#include<math.h>
+int main(){
+  int n;
+printf("enter number:\n");
+scanf("%d", &n);
+printf("%f", pow(n,2));
+return 0;
+}
